0dd9c0d7042b9a90cac621616350c55031c1acff
Anton Kurdin <anton.kurdin@gmail.com>
2021-07-23 19:49:29 +0300

    add to app such services as: add and remove notes, conditions for displaying, perm UpperCase in notes name

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const App = {
   data() {
     return {
-      counter: 0,
+      placeholderNote: 'Input note name',
       title: 'Notes List',
+      notes: [],
+      inputNote: '',
     }
-  }
+  },
+  methods: {
+    addNewNote() {
+      if (this.inputNote !== '') {
+        this.notes.push(this.inputNote);
+        this.inputNote = '';
+      }
+    },
+    removeNote(index) {
+      this.notes.splice(index, 1);
+    },
+    toUpperCase(note) {
+      return note.toUpperCase();
+    },
+  },
 }
 
 Vue.createApp(App).mount('#app')
